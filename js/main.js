0248e6f790bc101de6709db7e702451a7a399d5c
kwonsean <gownn12@naver.com>
2021-10-01 21:05:12 +0900

    feat: Add promotion part function
    - 프로모션 부분 기능  추가

--- js/main.js
+++ js/main.js
@@ -44,3 +44,44 @@ fadeEls.forEach(function(fadeEl, index){
     opacity: 1
   })
 });
+
+//notice 부분
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container', {
+  direction:'vertical',
+  autoplay: true,
+  loop: true,
+});
+
+// promotion 부분
+new Swiper('.promotion .swiper-container',{
+  // direction: 'horizontal', 기본값
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides : true, // 1번 슬라이드가 가운데 보이기
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true, // 사용자의 페이지 번호 요서 제어 가능 여부
+  },
+  navigation:{
+    prevEl:'.promotion .swiper-prev',
+    nextEl:'.promotion .swiper-next'
+  }
+})
+
+// 프로모션 toggle 부분
+const promotionEl =document.querySelector('.promotion')
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function(){
+  isHidePromotion = !isHidePromotion
+  if(isHidePromotion){
+    promotionEl.classList.add('hide');
+  }else{
+    promotionEl.classList.remove('hide');
+  }
+})
