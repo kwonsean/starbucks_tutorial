ab7451c4757f8d731c9e3f6a754de17d89b8b9cc
kwonsean <gownn12@naver.com>
2021-10-01 17:30:27 +0900

    feat: Make function to badge animation
    - 뱃지 에니메이션을 위한 함수 추가

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 const searchEl = document.querySelector(".search")
 const searchInputEl = searchEl.querySelector('input')
 
-searchEl.addEventListener('click', function (){
+searchEl.addEventListener('click', function(){
   searchInputEl.focus()
 });
 
 searchInputEl.addEventListener('focus', function(){
   searchEl.classList.add('focused');
   searchInputEl.setAttribute('placeholder', '통합검색')
-})
+});
 
 searchInputEl.addEventListener('blur', function(){
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '')
-})
+});
+
+
+const badgeEl = document.querySelector("header .badges")
+// 함수가 우르르 실행되는것이 아니라 0.3초 간격으로 실행되도록 함
+window.addEventListener('scroll', _.throttle(function(){
+  // console.log(window.scrollY)
+  if(window.scrollY > 500){
+    // gsap.to(요소, 지속시간(s단위), 옵션);
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else{
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+},300));
+// _.throttle(함수, 시간(ms단위))
